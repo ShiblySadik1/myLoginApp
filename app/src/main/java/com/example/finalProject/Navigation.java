15ac75fdb584df5eac80b6e3802c698399cf25ca
hing0014 <71026874+hing0014@users.noreply.github.com>
2020-11-13 12:41:10 -0500

    Spinner activity created. Base classes and layouts created

---
+++ app/src/main/java/com/example/finalProject/Navigation.java
@@ -0,0 +1,37 @@
+package com.example.finalProject;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidlabs.R;
+
+public class Navigation extends AppCompatActivity
+{
+    private final Intent goHome = new Intent(Navigation.this, MainActivity.class);
+    private final Intent goToCovid = new Intent(Navigation.this, Covid.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        Spinner navSpinner = findViewById(R.id.navSpinner);
+        navSpinner.setOnItemClickListener((parent, view, position, id) ->
+        {
+            if(position == 0) startActivity(goHome);
+            else if(position == 1) startActivity(goToCovid);
+
+        });
+    }
+
+
+}
